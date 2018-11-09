f5022e7afc4a6b3ea7f88d86820d3212b3de42ec
DU-ds <kjcolins1759@gmail.com>
2018-11-09 11:43:01 -0500

    11-09-18 pointer practice

---
+++ pointerPractice.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+const int len = 5;
+
+int main()
+{
+    using namespace std;
+
+    int num[len] = {0,100,200,300,400};
+
+    const int *pNum = num;
+
+    for (int i = 0; i < len; i++)
+    {
+        cout <<  i << "\t" << *(pNum + i) << endl;
+    }
+    
+    cout << endl;
+    int *pNum2 = 0;
+    pNum2 = num;
+
+    for (int i = 0; i < len; i++)
+    {
+        cout <<  i << "\t" << *(pNum2 + i) << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < len; i++)
+    {
+        cout <<  i << "\t" << num[i] << endl;
+    }
+
+    return 0;
+}
+
+
+
